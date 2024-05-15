bc44e3b345a960b315c7f0eeaf15e4b318b9ee8d
edmaker89 <edmaker@gmail.com>
2024-05-15 10:06:29 -0300

    construindo o menu

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { HeroTitleComponent } from '../../components/hero-title/hero-title.component';
 import { DestakCardComponent } from "../../components/destak-card/destak-card.component";
 import { SmallCardComponent } from "../../components/small-card/small-card.component";
+import { MenuBarComponent } from "../../components/menu-bar/menu-bar.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    imports: [HeroTitleComponent, DestakCardComponent, SmallCardComponent]
+    imports: [HeroTitleComponent, DestakCardComponent, SmallCardComponent, MenuBarComponent]
 })
 export class HomeComponent {
 
